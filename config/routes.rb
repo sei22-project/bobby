e74d69102cd0e03db6d537f2438410f4d88ffd3c
Zachariah Chow <zachariahchow@gmail.com>
2020-05-19 17:01:20 +0800

    style: Edit white-space for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  get 'home/Index'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  get 'categories', to: 'categories#index'
-  get 'categories/:id', to: 'categories#show'
+
+  devise_for :users
+
+  get 'categories', to: 'categories#index', as: 'categories'
+  get 'categories/:id', to: 'categories#show', as: 'category'
+
   get 'games/:id', to: 'games#show'
   post 'games', to: 'games#create'
   put 'games/:id', to: 'games#update'
   delete 'games/:id', to: 'games#delete'
+
   get 'users/:id', to: 'users#show'
   get 'users/:id/edit', to: 'users#edit'
   put 'users/:id', to: 'users#update'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
