98acb98ff6bf0e1b8686f638bbcf7c9193b656af
Zachariah Chow <zachariahchow@gmail.com>
2020-05-19 16:36:19 +0800

    feat: Add prelim routes for Categories, Games, Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  get 'categories', to: 'categories#index'
+  get 'categories/:id', to: 'categories#show'
+  get 'games/:id', to: 'games#show'
+  post 'games', to: 'games#create'
+  put 'games/:id', to: 'games#update'
+  delete 'games/:id', to: 'games#delete'
+  get 'users/:id', to: 'users#show'
+  get 'users/:id/edit', to: 'users#edit'
+  put 'users/:id', to: 'users#update'
 end
