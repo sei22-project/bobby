20f6a83ce26e3668cb8d4f62eb6f7b367f8edc48
Zachariah Chow <zachariahchow@gmail.com>
2020-05-19 22:32:56 +0800

    feat: Add resources for messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
 
   get 'rooms/:id', to: 'rooms#show'
 
+  resources :messages
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
