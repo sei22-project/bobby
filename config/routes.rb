3c2487f3da2e372e0b5b69f81e16cd21f918164e
Zachariah Chow <zachariahchow@gmail.com>
2020-05-19 22:18:46 +0800

    feat: Add rooms/:id route

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   get 'users/:id/edit', to: 'users#edit'
   put 'users/:id', to: 'users#update'
 
+  get 'rooms/:id', to: 'rooms#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
