d3ae13e6f676856ce6d042eb225062bbf6dc2903
Zachariah Chow <zachariahchow@gmail.com>
2020-05-19 17:37:49 +0800

    feat: Add Message & Room models, run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_074722) do
+ActiveRecord::Schema.define(version: 2020_05_19_093711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,12 +33,29 @@ ActiveRecord::Schema.define(version: 2020_05_19_074722) do
     t.index ["category_id"], name: "index_games_on_category_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "requests", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "game_id"
     t.integer "status"
     t.index ["game_id"], name: "index_requests_on_game_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_rooms_on_game_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,4 +80,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_074722) do
     t.index ["user_id"], name: "index_users_games_on_user_id"
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
+  add_foreign_key "rooms", "games"
 end
