1eba5f4e1df7498eb7ce596e6e7b194ac0172c64
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-05-19 15:56:43 +0800

    Added migration for category. Created models for category, game and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_073044) do
+ActiveRecord::Schema.define(version: 2020_05_19_074722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "type"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "title"
