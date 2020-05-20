3cde2667f5c47bbecc374357639eda633593605f
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-05-20 22:46:27 +0800

    Fixed merge conflixt on views/layout/application

--- app/javascript/packs/category.js
+++ app/javascript/packs/category.js
@@ -1,6 +1,6 @@
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 
-const date_pick = flatpickr(".date-flatpick", {
+const date_pick = flatpickr(".sortdate-flatpick", {
     altInput: true
 }); // flatpickr
