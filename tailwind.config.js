81ce57c9ea9f9c590c661cbf77c58d9100e7a1ee
Kenan <xinkenan.11@hotmail.com>
2020-05-20 17:04:10 +0800

    :wrench: updated tailwind config to include google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      display: ['Titillium Web', 'sans-serif'],
+      body: ['Arimo', 'sans-serif']
+    },
     extend: {},
   },
   variants: {},
