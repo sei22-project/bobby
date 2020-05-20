24bb9e2b9ed7a48522f82285c16aeafbdaf8ac7a
Zachariah Chow <zachariahchow@gmail.com>
2020-05-21 02:06:04 +0800

    feat: Add links to controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
   def index
     @games = Game.all
     @categories = Category.all
+    #Edit links here for navbar
+    @links = [{:name => "Home", :path => root_path},{:name => "Host a Game", :path => new_game_path}, {:name => "Dashboard", :path => dashboard_path}]
   end
 
   def sort
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
   def index
     @games = Game.all
     @categories = Category.all
+    #Edit links here for navbar
+    @links = [{:name => "Host a Game", :path => new_game_path}, {:name => "Join a Game", :path => categories_path}, {:name => "Dashboard", :path => dashboard_path}]
   end
 end
