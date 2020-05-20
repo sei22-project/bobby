566aaac1cf30b48615a9797bfa27fdf974ed0649
Zachariah Chow <zachariahchow@gmail.com>
2020-05-20 11:11:31 +0800

    feat: Add check_user to Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :check_user
   def show
     ## This should show user dashboard.
+    current_user
   end
 
   def edit
@@ -8,4 +10,11 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  private
+    def check_user
+      if current_user != User.find(params[:id])
+        redirect_to root_path
+      end
+    end
 end
