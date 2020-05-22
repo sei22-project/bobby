e64c10a5142eeb386e702c4e83f54fba7581bdff
Kenan <xinkenan.11@hotmail.com>
2020-05-22 17:42:05 +0800

    added featured game controller method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,21 @@ class HomeController < ApplicationController
     #Edit links here for navbar
     @links = [{:name => "Host a Game", :path => new_game_path}, {:name => "Join a Game", :path => categories_path}, {:name => "Dashboard", :path => dashboard_path}]
   end
+
+  def featured_games
+    @featured_games =  Game.order(start: :desc).take(20)
+
+    if @featured_games
+      respond_to do |format|
+        format.js { render partial: 'home/featured_games'}
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "No games available yet."
+        format.js { render partial: 'home/featured_games'}
+      end
+    end
+    
+
+  end
 end
