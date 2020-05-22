6b0993eaef330b7a7ef2046a22a1fa1daaa30e73
Zachariah Chow <zachariahchow@gmail.com>
2020-05-22 17:23:54 +0800

    feat: Add user auth for Application Cable connection

--- app/javascript/channels/activity_channel.js
+++ app/javascript/channels/activity_channel.js
@@ -1,15 +1,16 @@
 import consumer from "./consumer"
 
 consumer.subscriptions.create("ActivityChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+    connected() {
+        // Called when the subscription is ready for use on the server
+        console.log('Connected to the Activity Channel')
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+        // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
+    received(data) {
+        // Called when there's incoming data on the websocket for this channel
+    }
 });
