a69924e03f6170556e6a97a8a9bb2983bd7f5fbb
Zachariah Chow <zachariahchow@gmail.com>
2020-05-22 20:22:54 +0800

    feat: Add sub unsub update user online

--- app/channels/activity_channel.rb
+++ app/channels/activity_channel.rb
@@ -1,11 +1,25 @@
 class ActivityChannel < ApplicationCable::Channel
   def subscribed
     stream_from "activity_channel"
+
+    if current_user
+
+      current_user.update(online: true)
+
+    end
+
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
     ActionCable.server.broadcast "activity_channel", user_email: current_user.email, user_id: current_user.id, status: 'offline'
+
+    if current_user
+
+      current_user.update(online: false)
+
+    end
+
   end
 
   def appear
