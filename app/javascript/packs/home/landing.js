12ce5a237690d5b33d418054727f0dd2a9d32d33
Kenan <xinkenan.11@hotmail.com>
2020-05-22 17:48:35 +0800

    configure glider.js

---
+++ app/javascript/packs/home/landing.js
@@ -0,0 +1,39 @@
+import Glide from '@glidejs/glide'
+import Rails from "@rails/ujs";
+// Required Core Stylesheet
+import "@glidejs/glide/src/assets/sass/glide.core";
+
+const breakPoints = {
+    1200: {
+      perView: 3
+    },
+
+    1080: {
+      perView: 2
+    },
+
+    500: {
+    
+      perView: 1
+    }
+
+}
+
+const glideOptions = {
+  type:         'carousel',
+  autoplay:           3000,
+  gap:                  20,
+  perView: 3,
+  breakpoints: breakPoints,
+}
+
+document.addEventListener("turbolinks:load",function(){
+
+  Rails.ajax({
+    type: 'get',
+    url: "/featured_games",
+    success: () => new Glide('.glide', glideOptions).mount(),
+    error: (err) => console.log(err)
+  })
+
+})
