bceddd2b5b5a4da6fe2d56bad4b29c9b90ffa006
Zachariah Chow <zachariahchow@gmail.com>
2020-05-22 20:52:23 +0800

    feat: Complete user activity (pending styles)

--- app/channels/activity_channel.rb
+++ app/channels/activity_channel.rb
@@ -3,27 +3,27 @@ class ActivityChannel < ApplicationCable::Channel
     stream_from "activity_channel"
 
     if current_user
-
-      current_user.update(online: true)
-
+      if current_user.online == false
+        current_user.update(online: true)
+        ActionCable.server.broadcast "activity_channel", user_email: current_user.email, user_id: current_user.id, status: 'online'
+      end
     end
 
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-    ActionCable.server.broadcast "activity_channel", user_email: current_user.email, user_id: current_user.id, status: 'offline'
 
     if current_user
-
-      current_user.update(online: false)
-
+      if current_user.online == true
+        ActionCable.server.broadcast "activity_channel", user_email: current_user.email, user_id: current_user.id, status: 'offline'
+        current_user.update(online: false)
+      end
     end
 
   end
 
   def appear
-    ActionCable.server.broadcast "activity_channel", user_email: current_user.email, user_id: current_user.id, status: 'online'
   end
 
 end
--- app/javascript/channels/activity_channel.js
+++ app/javascript/channels/activity_channel.js
@@ -3,7 +3,6 @@ import consumer from "./consumer"
 consumer.subscriptions.create("ActivityChannel", {
     connected() {
         // Called when the subscription is ready for use on the server
-        this.perform("appear")
     },
 
     disconnected() {
@@ -13,13 +12,17 @@ consumer.subscriptions.create("ActivityChannel", {
     received(data) {
         // Called when there's incoming data on the websocket for this channel
         if (data.status == 'online') {
+
             console.log(`${data.user_email} has connected to the Activity Channel`);
             const userEl = document.querySelector(`.room-participants__user[data-user-id="${data.user_id}"]`);
             if (userEl) userEl.classList.remove("opacity-50");
-        } else {
+
+        } else if (data.status == 'offline') {
+
             console.log(`${data.user_email} has disconnected from the Activity Channel`);
             const userEl = document.querySelector(`.room-participants__user[data-user-id="${data.user_id}"]`);
             if (userEl) userEl.classList.add("opacity-50");
+
         }
     }
 });
