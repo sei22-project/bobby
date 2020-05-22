1ff19079500db3131fade7fb6668ebbef390aab8
Zachariah Chow <zachariahchow@gmail.com>
2020-05-23 03:30:23 +0800

    feat: Add create notification for create requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,18 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  after_commit :create_notifications, on: :create
+
+  private
+
+  def create_notifications
+    Notification.create do |notification|
+      notification.notify_type = 'post'
+      notification.actor = self.user
+      notification.user = User.find(self.game.host_id)
+      notification.target = self
+      notification.second_target = self.game
+    end
+  end
 end
