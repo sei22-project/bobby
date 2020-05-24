5604c9101570a69726a7d3c0c5faa9abe07d6348
Zachariah Chow <zachariahchow@gmail.com>
2020-05-24 22:49:39 +0800

    feat: Add basic styles / DOM calls for profile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,13 @@ require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
+import "./profile";
 import "@fortawesome/fontawesome-pro/js/all";
 
 // main css import
 import '../stylesheets/application.scss';
 import 'flatpickr/dist/flatpickr.min.css';
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/packs/profile.js
@@ -0,0 +1,21 @@
+if (document.querySelector('.profile__edit-btn')) {
+    const editProfileBtn = document.querySelector('.profile__edit-btn');
+    const updateProfileBtn = document.querySelector('.profile__update-btn');
+    const nameField = document.querySelector('.name__field');
+    const avatarField = document.querySelector('.avatar__field');
+
+    const avatarFileInput = document.querySelector('.avatar__file-input');
+    const avatarInputDisplay = document.querySelector('.avatar__input-display');
+
+    editProfileBtn.addEventListener('click', () => {
+        editProfileBtn.classList.add('hidden');
+        updateProfileBtn.classList.remove('hidden');
+        nameField.classList.add('border-b');
+        avatarField.classList.remove('hidden');
+    })
+
+    avatarFileInput.addEventListener('change', (ev) => {
+        avatarInputDisplay.innerText = ev.target.value;
+        avatarInputDisplay.classList.remove('hidden');
+    })
+}
