38ab89a618a163290d2af3c1503c408b005b221e
Zachariah Chow <zachariahchow@gmail.com>
2020-05-24 15:27:26 +0800

    fix: Add cloudflare proxy to production rb (test issue in prod)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
-require 'cloudflare_proxy'
+require "json"
+
+class CloudflareProxy
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    return @app.call(env) unless env["HTTP_CF_VISITOR"]
+
+    env["HTTP_X_FORWARDED_PROTO"] = JSON.parse(env["HTTP_CF_VISITOR"])["scheme"]
+    @app.call(env)
+  end
+end
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
