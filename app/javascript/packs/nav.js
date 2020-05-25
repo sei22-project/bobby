98b7d3e35223cbb7e11ad185883d69b5632de6ca
Kenan <xinkenan.11@hotmail.com>
2020-05-25 12:30:08 +0800

    removed test console.log

--- app/javascript/packs/nav.js
+++ app/javascript/packs/nav.js
@@ -1,7 +1,6 @@
 document.addEventListener('turbolinks:load', () => {
   const menuToggler = document.getElementsByClassName('js-menu-toggle')[0];
   const navbarMenu = document.getElementById('navbar-menu');
-  console.log(menuToggler)
 
   if (menuToggler && navbarMenu) {
     menuToggler.addEventListener('click', () => {
