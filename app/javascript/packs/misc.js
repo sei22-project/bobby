2528d1b0ecef978ca7a553b0c48164e75e2bf768
Kenan <xinkenan.11@hotmail.com>
2020-05-25 21:19:05 +0800

    added flash btn

---
+++ app/javascript/packs/misc.js
@@ -0,0 +1,37 @@
+function initNavBarMenu() {
+  const menuToggler = document.getElementsByClassName('js-menu-toggle')[0];
+  const navbarMenu = document.getElementById('navbar-menu');
+
+  if (menuToggler && navbarMenu) {
+    menuToggler.addEventListener('click', () => {
+      navbarMenu.classList.toggle('is_open');
+    });
+
+    window.addEventListener('mouseup', function (event) {
+      if (
+        event.target != navbarMenu &&
+        event.target != menuToggler &&
+        event.target.parentNode != navbarMenu
+      ) {
+        navbarMenu.classList.remove('is_open');
+      }
+    });
+  }
+}
+
+function initFlashMsgCloseable() {
+  const flashMsgCloseBtns = Array.from(document.getElementsByClassName('flash_message-close_btn'));
+  const flashMsgCloseBtnClicked = e => {
+    e.stopPropagation();
+    if (e.target.parentNode.matches('.flash_message')) {
+      e.target.parentNode.remove()
+    }
+  }
+  const addClickHandler = elem => elem.addEventListener('click', flashMsgCloseBtnClicked)
+  flashMsgCloseBtns && flashMsgCloseBtns.map(addClickHandler)
+}
+
+document.addEventListener('turbolinks:load', () => {
+  initNavBarMenu();
+  initFlashMsgCloseable();
+});
