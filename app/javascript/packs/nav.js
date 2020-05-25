8a4896403d50abd7921b7924c284a5dd72c62e8d
Kenan <xinkenan.11@hotmail.com>
2020-05-25 11:36:32 +0800

    fixed menu not working

--- app/javascript/packs/nav.js
+++ app/javascript/packs/nav.js
@@ -1,10 +1,12 @@
 document.addEventListener('turbolinks:load', () => {
   const menuToggler = document.getElementsByClassName('js-menu-toggle')[0];
   const navbarMenu = document.getElementById('navbar-menu');
+  console.log(menuToggler)
 
   if (menuToggler && navbarMenu) {
-    menuToggler.addEventListener('click', () =>
-      navbarMenu.classList.toggle('is_open')
+    menuToggler.addEventListener('click', () => {
+      navbarMenu.classList.toggle('is_open');
+      }
     );
 
     window.addEventListener('mouseup', function (event) {
