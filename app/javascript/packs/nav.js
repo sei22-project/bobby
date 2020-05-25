860d793217b1390af9b2180a7c844e9f7afe2035
Kenan <xinkenan.11@hotmail.com>
2020-05-25 10:26:15 +0800

    added menu toggle script

---
+++ app/javascript/packs/nav.js
@@ -0,0 +1,20 @@
+document.addEventListener('turbolinks:load', () => {
+  const menuToggler = document.getElementsByClassName('js-menu-toggle')[0];
+  const navbarMenu = document.getElementById('navbar-menu');
+
+  if (menuToggler && navbarMenu) {
+    menuToggler.addEventListener('click', () =>
+      navbarMenu.classList.toggle('is_open')
+    );
+
+    window.addEventListener('mouseup', function (event) {
+      if (
+        event.target != navbarMenu &&
+        event.target != menuToggler &&
+        event.target.parentNode != navbarMenu
+      ) {
+        navbarMenu.classList.remove('is_open');
+      }
+    });
+  }
+});
