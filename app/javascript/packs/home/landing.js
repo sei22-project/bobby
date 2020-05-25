97e3f58547542e57cb3945f86fe4c1558fb6869d
Zachariah Chow <zachariahchow@gmail.com>
2020-05-25 21:11:20 +0800

    styles: Change indentation trivial

--- app/javascript/packs/home/landing.js
+++ app/javascript/packs/home/landing.js
@@ -5,40 +5,40 @@ import '@glidejs/glide/src/assets/sass/glide.core.scss';
 import '@glidejs/glide/src/assets/sass/glide.theme.scss';
 
 const breakPoints = {
-  1200: {
-    perView: 3,
-  },
+    1200: {
+        perView: 3,
+    },
 
-  1080: {
-    perView: 2,
-  },
+    1080: {
+        perView: 2,
+    },
 
-  500: {
-    perView: 1,
-  },
+    500: {
+        perView: 1,
+    },
 };
 
 const glideOptions = {
-  type: 'carousel',
-  autoplay: 3000,
-  gap: 20,
-  perView: 3,
-  breakpoints: breakPoints,
+    type: 'carousel',
+    autoplay: 3000,
+    gap: 20,
+    perView: 3,
+    breakpoints: breakPoints,
 };
 
 const getDataAndInitCarousel = () => {
-  Rails.ajax({
-    type: 'get',
-    url: '/featured_games',
-    success: () => new Glide('.glide', glideOptions).mount(),
-    error: (err) => console.log(err),
-  });
+    Rails.ajax({
+        type: 'get',
+        url: '/featured_games',
+        success: () => new Glide('.glide', glideOptions).mount(),
+        error: (err) => console.log(err),
+    });
 };
 
 const startCarousel = () => {
-  if (document.getElementById('featured_games')) {
-    getDataAndInitCarousel();
-  }
+    if (document.getElementById('featured_games')) {
+        getDataAndInitCarousel();
+    }
 };
 
 document.addEventListener('turbolinks:load', startCarousel);
