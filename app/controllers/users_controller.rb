45b725f38a36f83773757a9e8ea623d078076270
Kenan <xinkenan.11@hotmail.com>
2020-05-25 13:36:30 +0800

    added group by day helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   before_action :check_user
+  helper_method :group_by_day
 
   def show_dashboard
-    ## This should show user dashboard.
-    @participating_games = current_user.games
-    @hosting_games = Game.where(host_id: current_user.id)
+
+
+    @participating_games = group_by_day(current_user.games)
+    @hosting_games = group_by_day(Game.where(host_id: current_user.id))
     #Edit links here for navbar
     @links = [{:name => "Home", :path => root_path}, {:name => "Host a Game", :path => new_game_path}, {:name => "Join a Game", :path => categories_path}]
 
@@ -33,4 +35,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :avatar)
     end
+
+    def group_by_day(obj) 
+      sort_by_day = -> ( d ) {d.start.strftime("%Y-%m-%d")}
+      return obj.group_by(&sort_by_day)
+    end
+
+
+
 end
