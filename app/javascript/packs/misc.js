29ac6936d9dd18a5558f7c241c842698879e5fa7
Zachariah Chow <zachariahchow@gmail.com>
2020-05-26 22:22:55 +0800

    fix: Fix datepicker js pack tag to async

--- app/javascript/packs/misc.js
+++ app/javascript/packs/misc.js
@@ -1,37 +1,43 @@
 function initNavBarMenu() {
-  const menuToggler = document.getElementsByClassName('js-menu-toggle')[0];
-  const navbarMenu = document.getElementById('navbar-menu');
+    const menuToggler = document.getElementsByClassName('js-menu-toggle')[0];
+    const navbarMenu = document.getElementById('navbar-menu');
 
-  if (menuToggler && navbarMenu) {
-    menuToggler.addEventListener('click', () => {
-      navbarMenu.classList.toggle('is_open');
-    });
+    if (menuToggler && navbarMenu) {
+        menuToggler.addEventListener('click', () => {
+            navbarMenu.classList.toggle('is_open');
+        });
 
-    window.addEventListener('mouseup', function (event) {
-      if (
-        event.target != navbarMenu &&
-        event.target != menuToggler &&
-        event.target.parentNode != navbarMenu
-      ) {
-        navbarMenu.classList.remove('is_open');
-      }
-    });
-  }
+        window.addEventListener('mouseup', function(event) {
+            if (
+                event.target != navbarMenu &&
+                event.target != menuToggler &&
+                event.target.parentNode != navbarMenu
+            ) {
+                navbarMenu.classList.remove('is_open');
+            }
+        });
+    }
 }
 
 function initFlashMsgCloseable() {
-  const flashMsgCloseBtns = Array.from(document.getElementsByClassName('flash_message-close_btn'));
-  const flashMsgCloseBtnClicked = e => {
-    e.stopPropagation();
-    if (e.target.parentNode.matches('.flash_message')) {
-      e.target.parentNode.remove()
+    const flashMsgCloseBtns = Array.from(document.getElementsByClassName('flash_message-close_btn'));
+    const flashMsgCloseBtnClicked = e => {
+        e.stopPropagation();
+        if (e.target.parentNode.matches('.flash_message')) {
+            e.target.parentNode.remove()
+        }
     }
-  }
-  const addClickHandler = elem => elem.addEventListener('click', flashMsgCloseBtnClicked)
-  flashMsgCloseBtns && flashMsgCloseBtns.map(addClickHandler)
+    const addClickHandler = elem => elem.addEventListener('click', flashMsgCloseBtnClicked)
+    flashMsgCloseBtns && flashMsgCloseBtns.map(addClickHandler)
+
+    if (document.querySelector('.jumbotron') && document.querySelector('.flash_message')) {
+        document.querySelector('.flash_message').classList.add('top-0');
+        document.querySelector('.flash_message').classList.add('left-0');
+        document.querySelector('.flash_message').classList.add('w-screen');
+    }
 }
 
 document.addEventListener('turbolinks:load', () => {
-  initNavBarMenu();
-  initFlashMsgCloseable();
+    initNavBarMenu();
+    initFlashMsgCloseable();
 });
